644accfc2cf5f0fac7a904b7d7ae9959a5e2ee61
julia semenuk <julia.semenuk@gmail.com>
2023-04-30 14:57:29 +0300

    update new version

--- english_level_app.py
+++ english_level_app.py
@@ -2,23 +2,27 @@
 # coding: utf-8
 
 import os
-import re
 import time
 
 import streamlit as st
 import pandas as pd
 
-from joblib import load
+import pysrt
+import chardet
 
-from detect_english_level import DetectEnglishLevel
+from joblib import load
+from english_level_process import ProcessData
 
 ENGLISH_LEVELS = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2']
 EXAMPLE_SUBS   = 'Fear.2023.720p.WEBRip.x264.AAC-[HQCINEMAS.COM].srt'
+DIRNAME = os.path.dirname(__file__)
 level = 1
 
-model = load(os.path.dirname(__file__) + '/model_english_level.joblib')
-detector = DetectEnglishLevel()
+# загружаем модель и класс с предобработкой
+model = load(DIRNAME + '/english_level_model.joblib')
+process = ProcessData()
 
+# шапка страницы
 st.set_page_config(page_title='Уровень английского в субтитрах', 
                    page_icon='📺', 
                    layout="centered", 
@@ -27,35 +31,43 @@ st.set_page_config(page_title='Уровень английского в субт
 
 with st.container():
     st.header('Определим уровень английского')
-    st.markdown('*по субтитрам 📺 в фильме*')
+    st.markdown('*по субтитрам* 📺 *в фильме*')
     
 uploaded_file = st.file_uploader('Загрузите файл с субтитрами в формате .srt', type='.srt')
 
 if uploaded_file is not None:
-    content = uploaded_file.getvalue()
+    # если файл загружен, то определяем кодировку, декодируем и передаем в pysrt
+    encoding = chardet.detect(uploaded_file.getvalue())['encoding']
+    content = pysrt.from_string(uploaded_file.getvalue().decode(encoding))
     filename = uploaded_file.name
 else:
-    file = open(f'{os.path.dirname(__file__)}/{EXAMPLE_SUBS}', 'rb')
-    content = file.read()
-    file.close()
+    # по умолчанию показываем демо-файл
     st.write('Например')
     filename = EXAMPLE_SUBS
+    fullpath = os.path.join(DIRNAME,filename)
+    encoding = chardet.detect(open(fullpath, "rb").read())['encoding']
+    content = pysrt.open(fullpath, encoding=encoding)
 
+# обработка файла и предсказание
 st.subheader(filename)
 with st.spinner('Рассчитываем...'):
-    subs = detector.process_srt(content)
-    if subs:
-        movies = detector.process_data(subs)
-        level = model.predict(movies)[0]
+    movies = process.process_data(content)
+    if len(movies)>0:
+        level  = model.predict(movies)[0]
+    else:
+        filename = EXAMPLE_SUBS
 
+
+'---'
+# расшифровываем предикт в юзер-френдли форму и выводим на страницу
 current_level = ENGLISH_LEVELS[min(round(level)-1, len(ENGLISH_LEVELS)-1)]
 floor_level   = ENGLISH_LEVELS[min(int(level)-1, len(ENGLISH_LEVELS)-1)]
-up_level      = ENGLISH_LEVELS[min(int(level), len(ENGLISH_LEVELS)-1)]
+ceil_level    = ENGLISH_LEVELS[min(int(level), len(ENGLISH_LEVELS)-1)]
 sub_level     = '+' if 0.2 <=level%1<0.5 or level>len(ENGLISH_LEVELS)-1 \
-                    else '+/'+up_level if 0.5<=level%1<0.8 \
+                    else '+/'+ceil_level if 0.5<=level%1<0.8 \
                     else ''
 user_level    = floor_level + sub_level
-'---'
+
 st.subheader('Уровень сложности')
 st.write(f'Для оценки мы используем опыт специалистов и словари Oxford. '
          f'Наши преподаватели рекомендуют фильм для изучения языка на уровне '
@@ -69,15 +81,16 @@ for i in range(min(round(1/6 * level * 100),101)):
 
 
 '---'
+# подсчитаем уровни слов по оксфорду
+words_under_level = movies[[l + 'ratio' for l in ENGLISH_LEVELS[:min(round(level),5)]]].sum(axis=1)[0]
+words_upper_level = movies[[l + 'ratio' for l in ENGLISH_LEVELS[round(level):5]]].sum(axis=1)[0]
+
 st.subheader('Стандарт CEFR')
 st.write('В словарях Oxford 3000 и 5000 — наиболее важные слова, которые должен знать каждый, кто учит английский.')
 st.write('Словарь Oxford соответствует стандартам CEFR – The Common European Framework of Reference for Languages. '
          'Это общеевропейские компетенции владения иностранным языком: изучение, преподавание, оценка — '
          'система уровней владения иностранным языком, используемая в Европейском Союзе.')
 st.write('Мы рассчитали, слова какого уровня чаще всего встречаются в фильме:')
-
-words_under_level = movies[[l + 'ratio' for l in ENGLISH_LEVELS[:min(round(level),5)]]].sum(axis=1)[0]
-words_upper_level = movies[[l + 'ratio' for l in ENGLISH_LEVELS[round(level):5]]].sum(axis=1)[0]
 
 if words_under_level > 0:
     st.progress(words_under_level, text=f"{words_under_level:.0%} слов до {current_level} уровня включительно")
@@ -88,28 +101,31 @@ if words_upper_level > 0:
 st.write('остальные слова не имеют определенного уровня.')
     
 '---'
+# подсчитаем длину слов
+less_than_4 = movies.less_than_4ratio[0]
+between_4_6 = movies[['len_equal_' + str(i) + 'ratio' for i in range(4,7)]].sum(axis=1)[0]
+more_than_6 = movies.more_than_6ratio[0]
+
 st.subheader('Запас слов')
 st.write(f'Всего в фильме встречается {movies.lemmas_unique[0]} разных слов.')
 st.write('В целом герои фильма испольуют слова вот такой длины:')
 
-less_than_3 = movies.less_than_3ratio[0]
-between_4_6 = movies[['equal_' + str(i) + 'ratio' for i in range(4,7)]].sum(axis=1)[0]
-more_than_7 = movies.more_than_7ratio[0]
-
-if less_than_3 > 0:
-    st.progress(less_than_3, text=f"Короткие слова не более трех букв: {less_than_3:.0%}")
+if less_than_4 > 0:
+    st.progress(less_than_4, text=f"Короткие слова не более трех букв: {less_than_4:.0%}")
 
 if between_4_6 > 0:    
     st.progress(between_4_6, text=f"Средние слова от четырех до шести букв: {between_4_6:.0%}")
     
-if more_than_7 > 0:
-    st.progress(more_than_7, text=f"Длинные слова от семи и больше букв: {movies.more_than_7ratio[0]:.0%}")
+if more_than_6 > 0:
+    st.progress(more_than_6, text=f"Длинные слова от семи и больше букв: {more_than_6:.0%}")
 
 '---'
+# подвал
 input_text = st.text_area('Было полезно?', placeholder='Поделитесь предложениями, пожеланиями, замечаниями...')
 if st.button("Отправить"):
-    st.write('На самом деле тут никакого действия не запланировано 😎 '
-             'пишите письма [на гитхабе](https://github.com/artefucktor)')
+    st.write('На самом деле тут никакого действия не запланировано 😎')
+    st.markdown('- Пишите письма [в телеграме](https://t.me/artefucktor)')
+    st.markdown('- или заходите [на гитхаб](https://github.com/artefucktor)')
 
 
 
---
+++ english_level_process.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import warnings
+import re
+import os
+
+import pandas as pd
+from joblib import load
+
+import spacy
+import pysrt
+
+class ProcessData():
+    
+    def __init__(self):
+        self.ENGLISH_LEVELS = ['A1', 'A2', 'B1', 'B2', 'C1']
+        self.oxford_words = load(os.path.dirname(__file__) + '/english_level_oxford.joblib')
+        self.nlp = spacy.load("en_core_web_sm")
+    
+    # process line
+    def process_line(self, line):
+        if re.search(r'[A-Za-z]',line): 
+            line = line.lower()
+            line = re.sub(r'\n', ' ', line)                            # remove new lines
+            line = re.sub(r'- ', ' ', line)                            # remove dash
+            line = re.sub(r'\<[^\<]+?\>', '', line)                    # remove html tags
+            line = re.sub(r'\([^\(]+?\)', '', line)                    # remove () parenthesis
+            line = re.sub(r'\[[^\[]+?\]', '', line)                    # remove [] parenthesis
+            line = re.sub(r'^([\w#\s]+\:)', ' ', line)                 # remove speaker tag
+            line = re.sub(r'[^[:alnum:][:punct:][:blank:]]',' ', line) # remove all other non-speach shars
+            line = re.sub(r'\s\s+', ' ', line).strip()                 # remove extra spaces
+        return line
+
+
+    # process text line by line   
+    def process_text(self, content):
+        text = []
+        duration  = []
+        charsrate = []
+        wordsrate  = []
+        for item in content:
+            if item.duration.ordinal>0:
+                line = self.process_line(item.text_without_tags)
+                text.append(line)
+                duration.append(item.duration.to_time())
+                charsrate.append(item.characters_per_second)
+                wordsrate.append(len(line.split())/item.duration.ordinal*1000.0)
+        return ' '.join(text), duration, charsrate, wordsrate
+
+
+    def count_words_by_level(self, row):
+        # oxford words in movies lemmas
+        row_lemmas = row.lemmas.split()
+        total_score = self.oxford_words.loc[self.oxford_words.word.isin(row_lemmas)].groupby('level').word.count().to_dict()
+        row[total_score.keys()] = total_score
+        return row
+    
+    
+    def process_data(self, content):
+        
+        movies = pd.DataFrame()
+        result = self.process_text(content)               # process file
+        if result:                                        # add movie to dataframe
+            subs, duration, charsrate, wordsrate = result
+            movies = pd.DataFrame({
+                'content'   : subs,
+                'duration'  : [duration],
+                'charsrate' : [charsrate],
+                'wordsrate' : [wordsrate],
+            },index=[0])
+            movies['charsrate'] = [charsrate]
+            movies['wordsrate'] = [wordsrate]
+            movies['content']   = subs
+            movies['target']    = 0
+            for col in self.ENGLISH_LEVELS + [l+'ratio' for l in self.ENGLISH_LEVELS]:
+                movies[col] = None
+                        
+            # lemmatization
+            movies['lemmas'] = movies.content.transform(lambda text: 
+                                                        ' '.join([w.lemma_.strip() 
+                                                                  for w in self.nlp(re.sub(r'[^a-z\s\']', '', text))
+                                                                 ])
+                                                       )
+            
+            # total lemmas count
+            # and unique lemmas count
+            # and unique ratio
+            movies['lemmas_count']  = movies.lemmas.transform(lambda x: len(x.split()))
+            movies['lemmas_unique'] = movies.lemmas.transform(lambda x: len(set(x.split())))
+            movies['lemmas_unique_ratio'] = movies.lemmas_unique / movies.lemmas_count
+            
+            # count oxford words by levels
+            movies = movies.apply(self.count_words_by_level, axis=1)
+
+            # ratio oxford words in content
+            for l in self.ENGLISH_LEVELS:
+                movies[l+'ratio'] = movies[l]/movies.lemmas_unique
+
+            # count words by length
+            # and ratio to total count
+            for i in range(1,10):
+                equal_col = 'len_equal_'+str(i)
+                more_col  = 'more_than_'+str(i)
+                less_col  = 'less_than_'+str(i)
+
+                movies[equal_col] = movies.lemmas.transform(lambda x: sum(len(word)==i for word in x.split()))
+                movies[more_col]  = movies.lemmas.transform(lambda x: sum(len(word)>i for word in x.split()))
+                movies[less_col]  = movies.lemmas.transform(lambda x: sum(len(word)<i for word in x.split()))
+
+                movies[equal_col + 'ratio'] = movies[equal_col] / movies.lemmas_count
+                movies[more_col + 'ratio']  = movies[more_col] / movies.lemmas_count
+                movies[less_col + 'ratio']  = movies[less_col] / movies.lemmas_count
+
+            movies = movies.fillna(0)
+
+        return movies
